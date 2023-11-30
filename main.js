b1c3d6907c23c1237eafce57df7039081a6be9ba
Виктор Брылёв <msne1488@gmail.com>
2023-11-30 15:08:51 +0300

    For checking

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
 (() => {
 	function debounce(fn, delay) {
 		let timer;
-		return function(...args) {
+		return function (...args) {
 			clearTimeout(timer);
 			timer = setTimeout(() => {
-				fn.apply(this, args)
-			}, delay)
-		}
+				fn.apply(this, args);
+			}, delay);
+		};
 	}
 
 	class View {
 		constructor() {
 			this.app = document.querySelector('.container');
 			this.form = this.createElement('form', 'search-form');
-			this.form.setAttribute('action', '#')
+			this.form.setAttribute('action', '#');
 			this.input = this.createElement('input', 'search-form__input');
-
 			this.searchReposList = this.createElement('ul', 'search-list', 'list-reset');
-			this.reposList = this.createElement('ul', 'repositories', 'list-reset')
+			this.reposList = this.createElement('ul', 'repositories', 'list-reset');
 
 			this.form.appendChild(this.input);
 			this.form.appendChild(this.searchReposList);
 			this.app.appendChild(this.form);
-			this.app.appendChild(this.reposList)
+			this.app.appendChild(this.reposList);
 		}
 
 		createElement(elementTag, ...elementClass) {
 			const element = document.createElement(elementTag);
-			const classes = [...elementClass]
 			if (elementClass) {
-				classes.forEach((el) => {
+				[...elementClass].forEach((el) => {
 					element.classList.add(el);
-				})
+				});
 			}
 
 			return element;
@@ -40,54 +38,63 @@
 		createRepos(reposData) {
 			const repo = this.createElement('li', 'search-list__item');
 			repo.textContent = `${reposData.name}`;
-			this.searchReposList.appendChild(repo)
+			this.searchReposList.appendChild(repo);
 
 			repo.addEventListener('click', () => {
-				const addRepo = this.createElement('li', 'repositories__item');
-				const repoName = this.createElement('span', 'repositories__item-name');
-				repoName.textContent = `name: ${reposData.name}`;
-				const repoOwner = this.createElement('span', 'repositories__item-owner');
-				repoOwner.textContent = `owner: ${reposData.owner.login}`;
-				const repoStars = this.createElement('span', 'repositories__item-stars');
-				repoStars.textContent = `stars: ${reposData.size}`;
-
-				[repoName, repoOwner, repoStars].forEach(el => addRepo.appendChild(el))
-
+				const addRepo = this.createRepoItem(reposData);
 				this.reposList.appendChild(addRepo);
-			})
+				this.input.value = '';
+				this.searchReposList.innerHTML = '';
+			});
+		}
+
+		createRepoItem(reposData) {
+			const addRepo = this.createElement('li', 'repositories__item');
+			const repoName = this.createElement('p', 'repositories__item-descr');
+			repoName.textContent = `Name: ${reposData.name}`;
+			const repoOwner = this.createElement('p', 'repositories__item-descr');
+			repoOwner.textContent = `Owner: ${reposData.owner.login}`;
+			const repoStars = this.createElement('p', 'repositories__item-descr');
+			repoStars.textContent = `Stars: ${reposData.stargazers_count}`;
+			const deleteBtn = this.createElement('button', 'repositories__item-btn', 'btn-reset');
+
+			[repoName, repoOwner, repoStars, deleteBtn].forEach((el) => addRepo.appendChild(el));
+
+			deleteBtn.addEventListener('click', () => addRepo.remove());
+
+			return addRepo;
 		}
 	}
 
 	class Search {
 		constructor(view) {
 			this.view = view;
-
-			this.view.input.addEventListener('keyup', debounce(this.searchRepositories.bind(this), 400) );
+			this.debouncedSearch = debounce(this.searchRepositories.bind(this), 500);
+			this.view.input.addEventListener('input', this.debouncedSearch);
 		}
 
 		async searchRepositories() {
-			if(this.view.input.value === '') {
+			if (this.view.input.value === '') {
 				this.view.searchReposList.innerHTML = '';
 				return;
 			}
-			return await fetch(`https://api.github.com/search/repositories?q=${this.view.input.value}`)
-				.then((res) => {
-					if (res.ok) {
-						res.json().then((res) => {
-							const repos = res.items;
-							this.view.searchReposList.innerHTML = '';
-							
-							for (let i = 0; i < 5; i++) {
-								const element = repos[i];
-								this.view.createRepos(element)
-								console.log(element.name)
-								console.log(element)
-							}
-						})
-					}
-				})
+
+			try {
+				const response = await fetch(`https://api.github.com/search/repositories?q=${this.view.input.value}`);
+				if (response.ok) {
+					const res = await response.json();
+					const repos = res.items.slice(0, 5);
+					this.view.searchReposList.innerHTML = '';
+
+					repos.forEach((element) => {
+						this.view.createRepos(element);
+					});
+				}
+			} catch (error) {
+				console.error('Error fetching repositories:', error);
+			}
 		}
 	}
 
 	new Search(new View());
-})()
+})();
